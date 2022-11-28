1d061d18c22bcf0cebbcfc0eeaa79bdcee9441d2
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 17:32:56 +0100

    fix if(.lenght < .lenght)

--- jsSnack-3/script.js
+++ jsSnack-3/script.js
@@ -6,7 +6,7 @@ const arrDue= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 while(arrUno.length !== arrDue.length){
   const userPrompt= prompt("inserisci qualsiasi cosa");
-  if (arrUno < arrDue){
+  if (arrUno.length < arrDue.length){
     arrUno.push(userPrompt);
   }else{
     arrDue.push(userPrompt);
