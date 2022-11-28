83b801e91cf2e4d0855c0d79278ea49efcbb4cd9
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 16:24:22 +0100

    number from 1 to 10

---
+++ jsSnack-1/script.js
@@ -0,0 +1,9 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+let promptElement= parseInt(prompt("Inserisci un numero da 1 a 10"));
+
+while(promptElement<0 || promptElement>10 || Number.isNaN(promptElement)){
+  promptElement= parseInt(prompt("Inserisci SOLO un numero da 1 a 10"));
+}
+
+console.log(promptElement, "è il numero iserito")
