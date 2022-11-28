da5ddc322dd55d1e847c89fd37783370352e5da4
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 18:38:28 +0100

    alternate array

--- jsSnack-4/script.js
+++ jsSnack-4/script.js
@@ -1,1 +1,14 @@
-console.log("4")
+// Prendere due array della stessa lunghezza e generarne un terzo, prendendo alternativamente gli elementi da uno e dall'altro es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+const arrLetter= ["a", "b", "c", "e", "f"];
+
+const arrNumber= [1, 2, 3, 4, 5];
+
+const biggerArr= (arrLetter.length > arrNumber.length) ? arrLetter.length : arrNumber.length;
+
+let arrCombo= [];
+
+for (let i = 0; i < biggerArr; i++){
+  arrCombo.push(arrLetter[i], arrNumber[i])
+}
+
+console.log(arrCombo)
