d6bd834f310c6fc5222697bf7120b49fbd71bcde
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-28 17:06:58 +0100

    revome prompt before cycle for

--- jsSnack-2/script.js
+++ jsSnack-2/script.js
@@ -1,15 +1,13 @@
 // Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
-const myNumber= [];
-
-let promptElement= parseInt(prompt("inserisci un numero"));
+const myNumbers= [];
 
 let sum= 0;
 
 for (let i = 0; sum < 50; i++){
   promptElement= parseInt(prompt("inserisci un numero"))
-  myNumber.push(promptElement);
-  console.log(myNumber);
+  myNumbers.push(promptElement);
+  console.log(myNumbers);
   sum+= promptElement;
   console.log(sum);
 }
