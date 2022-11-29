047c887d30543e590d6e48f37e2dc715dc519397
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-29 12:36:19 +0100

    sum only numbers

--- jsSnack-6/script.js
+++ jsSnack-6/script.js
@@ -1,1 +1,24 @@
-console.log("6")
+/**
+ *
+ * Crea una funzione che dato un array, calcoli la somma dei suoi elementi.
+ *
+ */
+
+function getSum(array){
+  let sum = 0;
+
+  for(let i = 0; i< array.length; i++){
+
+    const fixNumb= parseInt(array[i], 10);
+
+    if (!Number.isNaN(fixNumb)){
+      sum += fixNumb;
+    }
+  };
+
+  return sum;
+};
+
+const list= [1, 2, 3, "ciao", "cioccolato", 120];
+
+console.log(getSum(list));
