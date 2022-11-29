f01caf74a8ff3b5de7eca059901a538c99d2f72c
albertobaggio96 <albertobaggio96@gmail.com>
2022-11-29 11:59:35 +0100

    function getRandomNumber

--- jsSnack-5/script.js
+++ jsSnack-5/script.js
@@ -1,1 +1,14 @@
-console.log("5")
+/**
+ * Crea una funzione che accetti due argomenti, e che restituisca un valore numerico randomico tra i due argomenti inclusi.
+ */
+
+function getRandomNumber(numMin, numMax){
+  const randomNumber= Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+  return randomNumber;
+}
+
+console.log(getRandomNumber(1, 10));
+
+const casualNamber= getRandomNumber(1, 10);
+
+console.warn(casualNamber);
