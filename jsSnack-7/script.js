e025660a9f07f1a193f48d3575873a53562f5909
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-01 11:37:05 +0100

    random  position array

--- jsSnack-7/script.js
+++ jsSnack-7/script.js
@@ -1,1 +1,30 @@
-console.log("7")
+/**
+ * Crea due tag div con due id diversi.
+ * Un div avrà il testo colorato di rosso mentre l'altro di verde.
+ * Partendo da un array di numeri,
+ * creiamo un nuovo elemento p e lo stampiamo nell'div rosso  se è un numero dispari
+ * e nel div verde se è un numero pari.
+ *
+ *
+ */
+
+ const array = [1,33,55,64,33,22,10,44];
+ // funzione che crei un nuovo elemento <p> </p>
+ // restituirlo a chi invoca la funzione
+ //
+ // ciclo per ogni elemento dell'array
+ // se l'elemento è un numero pari, crei nuovo elemento e lo aggiungo al div verde
+ // se l'elemento è un numero dispari, creo  nuovo elemento e lo aggiungo al div rosso
+
+ function getNewPElement(div){
+  const pElement= document.createElement("p");
+  return div.append(pElement);
+ }
+
+ let divEven= document.getElementById("even");
+ let divOdd= document.getElementById("odd");
+
+ divEven= getNewPElement(divEven);
+ divOdd= getNewPElement(divOdd);
+
+ 
--- jsSnack-8/script.js
+++ jsSnack-8/script.js
@@ -1,1 +1,26 @@
-console.log("8")
+/**
+ *
+ * Data una lista di 20 numeri diversi.
+ * generiamo una lista secondaria di
+ * 10 numeri presi dalla lista, diversi tra loro.
+ *
+ */
+ const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+ const newList= [];
+
+ function getRandomNumber(minNum, maxNum){
+  const random = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+  return random
+ }
+
+ while(newList.length < 10){
+  const randomListNumber= getRandomNumber(0, startingList.length-1);
+  
+  if (!newList.includes(startingList[randomListNumber])){
+    newList.push(startingList[randomListNumber]);
+  }
+ }
+
+ console.log(newList, "length " + newList.length)
+
