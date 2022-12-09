ca9876e62c5c1c040c8671a06124d6a2b9fb2e88
albertobaggio96 <albertobaggio96@gmail.com>
2022-12-09 18:37:46 +0100

    snack button ten

--- jsSnack-9/script.js
+++ jsSnack-9/script.js
@@ -1,1 +1,22 @@
-console.log("9")
+
+let button= document.querySelector("button");
+
+let sum = 0;
+
+  button.addEventListener("click", function ten(){
+    sum ++;
+  
+    button.innerHTML = `Play ${sum}`;
+  
+    if (sum === 10){
+      button.classList.add("red");
+      button.removeEventListener("click", ten);
+      setTimeout(() => {
+        button.classList.remove("red");
+        button.addEventListener("click", ten);
+        sum = 0
+        button.innerHTML = `Play`;
+      }, 3000);
+    }
+
+})
